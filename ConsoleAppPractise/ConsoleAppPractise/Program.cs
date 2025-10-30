b68186ee348d0983ae1cf8f5b80d7fd679445099
George Zacharias <zachariasg7@hotmail.gr>
2025-10-30 08:57:21 +0200

    initial commit

---
+++ ConsoleAppPractise/ConsoleAppPractise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+int vowelCount = 0;
+string takis = "takis tsan";
+foreach (char c in takis)
+{
+    char lower = char.ToLower(c);
+    if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u')
+    {
+        vowelCount++;
+    }
+    ;
+
+}
+Console.Write(vowelCount);
